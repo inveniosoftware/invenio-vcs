palkerecsenyi/invenio-vcs-new

feat: views layer

invenio_vcs/ext.py
@@ -1,0 +1,121 @@
+# -*- coding: utf-8 -*-
+# This file is part of Invenio.
+# Copyright (C) 2025 CERN.
+#
+# Invenio is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Invenio module that adds VCS integration to the platform."""
+
+from flask import current_app, request
+from flask_menu import current_menu
+from invenio_i18n import LazyString
+from invenio_i18n import gettext as _
+from invenio_theme.proxies import current_theme_icons
+from six import string_types
+from werkzeug.utils import cached_property, import_string
+
+from invenio_vcs.config import get_provider_config_override, get_provider_list
+from invenio_vcs.receivers import VCSReceiver
+from invenio_vcs.service import VCSRelease
+from invenio_vcs.utils import obj_or_import_string
+
+from . import config
+
+
+class InvenioVCS(object):
+    """Invenio-VCS extension."""
+
+    def __init__(self, app=None):
+        """Extension initialization."""
+        if app:
+            self.init_app(app)
+
+    @cached_property
+    def release_api_class(self):
+        """Release API class."""
+        cls = current_app.config["VCS_RELEASE_CLASS"]
+        if isinstance(cls, string_types):
+            cls = import_string(cls)
+        assert issubclass(cls, VCSRelease)
+        return cls
+
+    @cached_property
+    def release_error_handlers(self):
+        """Release error handlers."""
+        error_handlers = current_app.config.get("VCS_ERROR_HANDLERS") or []
+        return [
+            (obj_or_import_string(error_cls), obj_or_import_string(handler))
+            for error_cls, handler in error_handlers
+        ]
+
+    def init_app(self, app):
+        """Flask application initialization."""
+        self.init_config(app)
+        self.init_config_overrides(app)
+        app.extensions["invenio-vcs"] = self
+
+    def init_config(self, app):
+        """Initialize configuration."""
+        app.config.setdefault(
+            "VCS_SETTINGS_TEMPLATE",
+            app.config.get("SETTINGS_TEMPLATE", "invenio_vcs/settings/base.html"),
+        )
+
+        for k in dir(config):
+            if k.startswith("VCS_"):
+                app.config.setdefault(k, getattr(config, k))
+
+    def init_config_overrides(self, app):
+        """Update each provider to allow overriding its settings via a dict config variable."""
+        providers = get_provider_list(app)
+        for provider in providers:
+            config_override = get_provider_config_override(provider.id, app)
+            provider.update_config_override(config_override)
+
+
+def finalize_app_ui(app):
+    """Finalize app."""
+    init_menu(app)
+    init_webhooks(app)
+
+
+def finalize_app_api(app):
+    """Finalize app."""
+    init_webhooks(app)
+
+
+def init_menu(app):
+    """Init menu."""
+    for provider in get_provider_list(app):
+
+        def is_active(current_node):
+            return (
+                request.endpoint.startswith("invenio_vcs.")
+                and request.view_args.get("provider", "") == current_node.name
+            )
+
+        current_menu.submenu(f"settings.{provider.id}").register(
+            endpoint="invenio_vcs.get_repositories",
+            endpoint_arguments_constructor=lambda id=provider.id: {"provider": id},
+            text=_(
+                "%(icon)s %(provider)s",
+                icon=LazyString(
+                    lambda: f'<i class="{current_theme_icons[provider.icon]}"></i>'
+                ),
+                provider=provider.name,
+            ),
+            order=10,
+            active_when=is_active,
+        )
+
+
+def init_webhooks(app):
+    """Register the webhook receivers based on the configured VCS providers."""
+    state = app.extensions.get("invenio-webhooks")
+    if state is not None:
+        for provider in get_provider_list(app):
+            # Procedurally register the webhook receivers instead of including them as an entry point, since
+            # they are defined in the VCS provider config list rather than in the instance's setup.cfg file.
+            if provider.id not in state.receivers:
+                state.register(provider.id, VCSReceiver)

invenio_vcs/views/vcs.py
@@ -1,0 +1,220 @@
+# -*- coding: utf-8 -*-
+# This file is part of Invenio.
+# Copyright (C) 2014-2025 CERN.
+# Copyright (C) 2024 Graz University of Technology.
+# Copyright (C) 2024 KTH Royal Institute of Technology.
+#
+# Invenio is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""VCS views blueprint for Invenio platform."""
+
+from functools import wraps
+
+from flask import Blueprint, abort, current_app, render_template
+from flask_login import current_user, login_required
+from invenio_db import db
+from invenio_i18n import gettext as _
+from sqlalchemy.orm.exc import NoResultFound
+
+from invenio_vcs.service import VCSService
+
+from ..errors import RepositoryAccessError, RepositoryNotFoundError, VCSTokenNotFound
+
+
+def vcs_error_handler():
+    """Common error handling behaviour for VCS routes."""
+
+    def decorator(f):
+        @wraps(f)
+        def inner(*args, **kwargs):
+            try:
+                return f(*args, **kwargs)
+            except RepositoryAccessError:
+                abort(403)
+            except (NoResultFound, RepositoryNotFoundError):
+                abort(404)
+            except Exception as exc:
+                current_app.logger.exception(str(exc))
+                abort(400)
+
+        return inner
+
+    return decorator
+
+
+def require_vcs_connected():
+    """Requests an oauth session token to be configured for the user."""
+
+    def decorator(f):
+        @wraps(f)
+        def inner(*args, **kwargs):
+            provider = kwargs["provider"]
+            svc = VCSService.for_provider_and_user(provider, current_user.id)
+            if svc.is_authenticated:
+                return f(*args, **kwargs)
+            raise VCSTokenNotFound(
+                current_user, _("Account must be connected to the VCS provider.")
+            )
+
+        return inner
+
+    return decorator
+
+
+def create_ui_blueprint(app):
+    """Creates blueprint and registers UI endpoints if the integration is enabled."""
+    blueprint = Blueprint(
+        "invenio_vcs",
+        __name__,
+        static_folder="../static",
+        template_folder="../templates",
+        url_prefix="/account/settings/vcs/<provider>",
+    )
+    with app.app_context():  # Todo: Temporary fix, it should be removed when inveniosoftware/invenio-theme#355 is merged
+        register_ui_routes(blueprint)
+    return blueprint
+
+
+def create_api_blueprint(app):
+    """Creates blueprint and registers API endpoints if the integration is enabled."""
+    blueprint_api = Blueprint(
+        "invenio_vcs_api", __name__, url_prefix="/user/vcs/<provider>"
+    )
+    register_api_routes(blueprint_api)
+    return blueprint_api
+
+
+def register_ui_routes(blueprint):
+    """Register ui routes."""
+
+    @blueprint.route("/")
+    @login_required
+    @vcs_error_handler()
+    def get_repositories(provider):
+        """Display list of the user's repositories."""
+        svc = VCSService.for_provider_and_user(provider, current_user.id)
+        ctx: dict = dict(
+            connected=False,
+            provider=provider,
+            vocabulary=svc.provider.factory.vocabulary,
+            repo_url=svc.provider.factory.url_for_repository,
+            new_repo_url=svc.provider.factory.url_for_new_repo(),
+        )
+
+        if svc.is_authenticated:
+            # Generate the repositories view object
+            repos = svc.list_repositories()
+            last_sync = svc.get_last_sync_time()
+
+            ctx.update(
+                {
+                    "connected": True,
+                    "repos": repos,
+                    "last_sync": last_sync,
+                }
+            )
+
+        return render_template(current_app.config["VCS_TEMPLATE_INDEX"], **ctx)
+
+    @blueprint.route("/repository/<path:repo_id>")
+    @login_required
+    @require_vcs_connected()
+    @vcs_error_handler()
+    def get_repository(provider, repo_id):
+        """Displays one repository.
+
+        Retrieves and builds context to display all repository releases, if any.
+        """
+        svc = VCSService.for_provider_and_user(provider, current_user.id)
+
+        repo = svc.get_repository(repo_id)
+        latest_release = svc.get_repo_latest_release(repo)
+        default_branch = svc.get_repo_default_branch(repo_id)
+        releases = svc.list_repo_releases(repo)
+        release_url = svc.provider.factory.url_for_release
+        new_release_url = svc.provider.factory.url_for_new_release(repo.full_name)
+        new_citation_file_url = svc.provider.factory.url_for_new_file(
+            repo.full_name, default_branch or "main", "CITATION.cff"
+        )
+
+        return render_template(
+            current_app.config["VCS_TEMPLATE_VIEW"],
+            latest_release=latest_release,
+            provider=provider,
+            repo=repo,
+            releases=releases,
+            default_branch=default_branch,
+            release_url=release_url,
+            new_release_url=new_release_url,
+            new_citation_file_url=new_citation_file_url,
+            vocabulary=svc.provider.factory.vocabulary,
+        )
+
+
+def register_api_routes(blueprint):
+    """Register API routes."""
+
+    @login_required
+    @require_vcs_connected()
+    @vcs_error_handler()
+    @blueprint.route("/repositories/sync", methods=["POST"])
+    def sync_user_repositories(provider):
+        """Synchronizes user repos.
+
+        Currently:
+            POST /api/user/vcs/<provider>/repositories/sync
+        Previously:
+            POST /account/settings/github/hook
+        """
+        svc = VCSService.for_provider_and_user(provider, current_user.id)
+        svc.sync()
+        db.session.commit()
+
+        return "", 200
+
+    @login_required
+    @require_vcs_connected()
+    @vcs_error_handler()
+    @blueprint.route("/repositories/<repository_id>/enable", methods=["POST"])
+    def enable_repository(provider, repository_id):
+        """Enables one repository.
+
+        Currently:
+            POST /api/user/vcs/<provider>/repositories/<repository_id>/enable
+        Previously:
+            POST /account/settings/github/hook
+        """
+        svc = VCSService.for_provider_and_user(provider, current_user.id)
+        create_success = svc.enable_repository(repository_id)
+
+        db.session.commit()
+        if create_success:
+            return "", 201
+        else:
+            raise Exception(
+                _("Failed to enable repository, hook creation not successful.")
+            )
+
+    @login_required
+    @require_vcs_connected()
+    @vcs_error_handler()
+    @blueprint.route("/repositories/<repository_id>/disable", methods=["POST"])
+    def disable_repository(provider, repository_id):
+        """Disables one repository.
+
+        Currently:
+            POST /api/user/vcs/<provider>/repositories/<repository_id>/disable
+        Previously:
+            DELETE /account/settings/github/hook
+        """
+        svc = VCSService.for_provider_and_user(provider, current_user.id)
+        remove_success = svc.disable_repository(repository_id)
+
+        db.session.commit()
+        if remove_success:
+            return "", 204
+        else:
+            raise Exception(
+                _("Failed to disable repository, hook removal not successful.")
+            )

inveniosoftware/invenio-vcs

invenio_vcs/ext.py
@@ -1,0 +1,121 @@
+# -*- coding: utf-8 -*-
+# This file is part of Invenio.
+# Copyright (C) 2025 CERN.
+#
+# Invenio is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Invenio module that adds VCS integration to the platform."""
+
+from flask import current_app, request
+from flask_menu import current_menu
+from invenio_i18n import LazyString
+from invenio_i18n import gettext as _
+from invenio_theme.proxies import current_theme_icons
+from six import string_types
+from werkzeug.utils import cached_property, import_string
+
+from invenio_vcs.api import VCSRelease
+from invenio_vcs.config import get_provider_config_override, get_provider_list
+from invenio_vcs.receivers import VCSReceiver
+from invenio_vcs.utils import obj_or_import_string
+
+from . import config
+
+
+class InvenioVCS(object):
+    """Invenio-VCS extension."""
+
+    def __init__(self, app=None):
+        """Extension initialization."""
+        if app:
+            self.init_app(app)
+
+    @cached_property
+    def release_api_class(self):
+        """Release API class."""
+        cls = current_app.config["VCS_RELEASE_CLASS"]
+        if isinstance(cls, string_types):
+            cls = import_string(cls)
+        assert issubclass(cls, VCSRelease)
+        return cls
+
+    @cached_property
+    def release_error_handlers(self):
+        """Release error handlers."""
+        error_handlers = current_app.config.get("VCS_ERROR_HANDLERS") or []
+        return [
+            (obj_or_import_string(error_cls), obj_or_import_string(handler))
+            for error_cls, handler in error_handlers
+        ]
+
+    def init_app(self, app):
+        """Flask application initialization."""
+        self.init_config(app)
+        self.init_config_overrides(app)
+        app.extensions["invenio-vcs"] = self
+
+    def init_config(self, app):
+        """Initialize configuration."""
+        app.config.setdefault(
+            "VCS_SETTINGS_TEMPLATE",
+            app.config.get("SETTINGS_TEMPLATE", "invenio_vcs/settings/base.html"),
+        )
+
+        for k in dir(config):
+            if k.startswith("VCS_"):
+                app.config.setdefault(k, getattr(config, k))
+
+    def init_config_overrides(self, app):
+        """Update each provider to allow overriding its settings via a dict config variable."""
+        providers = get_provider_list(app)
+        for provider in providers:
+            config_override = get_provider_config_override(provider.id, app)
+            provider.update_config_with_override(config_override)
+
+
+def finalize_app_ui(app):
+    """Finalize app."""
+    init_menu(app)
+    init_webhooks(app)
+
+
+def finalize_app_api(app):
+    """Finalize app."""
+    init_webhooks(app)
+
+
+def init_menu(app):
+    """Init menu."""
+    for provider in get_provider_list(app):
+
+        def is_active(current_node):
+            return (
+                request.endpoint.startswith("invenio_vcs.")
+                and request.view_args.get("provider", "") == current_node.name
+            )
+
+        current_menu.submenu(f"settings.{provider.id}").register(
+            endpoint="invenio_vcs.get_repositories",
+            endpoint_arguments_constructor=lambda id=provider.id: {"provider": id},
+            text=_(
+                "%(icon)s %(provider)s",
+                icon=LazyString(
+                    lambda: f'<i class="{current_theme_icons[provider.icon]}"></i>'
+                ),
+                provider=provider.name,
+            ),
+            order=10,
+            active_when=is_active,
+        )
+
+
+def init_webhooks(app):
+    """Register the webhook receivers based on the configured VCS providers."""
+    state = app.extensions.get("invenio-webhooks")
+    if state is not None:
+        for provider in get_provider_list(app):
+            # Procedurally register the webhook receivers instead of including them as an entry point, since
+            # they are defined in the VCS provider config list rather than in the instance's setup.cfg file.
+            if provider.id not in state.receivers:
+                state.register(provider.id, VCSReceiver)

invenio_vcs/views/vcs.py
@@ -1,0 +1,221 @@
+# -*- coding: utf-8 -*-
+# This file is part of Invenio.
+# Copyright (C) 2014-2025 CERN.
+# Copyright (C) 2024 Graz University of Technology.
+# Copyright (C) 2024 KTH Royal Institute of Technology.
+#
+# Invenio is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""VCS views blueprint for Invenio platform."""
+
+from functools import wraps
+
+from flask import Blueprint, abort, current_app, render_template, request
+from flask_login import current_user, login_required
+from invenio_db import db
+from invenio_i18n import gettext as _
+from sqlalchemy.orm.exc import NoResultFound
+
+from invenio_vcs.service import VCSService
+
+from ..errors import RepositoryAccessError, RepositoryNotFoundError, VCSTokenNotFound
+
+
+def vcs_error_handler():
+    """Common error handling behaviour for VCS routes."""
+
+    def decorator(f):
+        @wraps(f)
+        def inner(*args, **kwargs):
+            try:
+                return f(*args, **kwargs)
+            except RepositoryAccessError:
+                abort(403)
+            except (NoResultFound, RepositoryNotFoundError):
+                abort(404)
+            except Exception as exc:
+                current_app.logger.exception(str(exc))
+                abort(400)
+
+        return inner
+
+    return decorator
+
+
+def require_vcs_connected():
+    """Requests an oauth session token to be configured for the user."""
+
+    def decorator(f):
+        @wraps(f)
+        def inner(*args, **kwargs):
+            provider = kwargs["provider"]
+            svc = VCSService.for_provider_and_user(provider, current_user.id)
+            if svc.is_authenticated:
+                return f(*args, **kwargs)
+            raise VCSTokenNotFound(
+                current_user, _("Account must be connected to the VCS provider.")
+            )
+
+        return inner
+
+    return decorator
+
+
+def create_ui_blueprint(app):
+    """Creates blueprint and registers UI endpoints if the integration is enabled."""
+    blueprint = Blueprint(
+        "invenio_vcs",
+        __name__,
+        static_folder="../static",
+        template_folder="../templates",
+        url_prefix="/account/settings/vcs/<provider>",
+    )
+    with app.app_context():  # Todo: Temporary fix, it should be removed when inveniosoftware/invenio-theme#355 is merged
+        register_ui_routes(blueprint)
+    return blueprint
+
+
+def create_api_blueprint(app):
+    """Creates blueprint and registers API endpoints if the integration is enabled."""
+    blueprint_api = Blueprint(
+        "invenio_vcs_api", __name__, url_prefix="/user/vcs/<provider>"
+    )
+    register_api_routes(blueprint_api)
+    return blueprint_api
+
+
+def register_ui_routes(blueprint):
+    """Register ui routes."""
+
+    @blueprint.route("/")
+    @login_required
+    @vcs_error_handler()
+    def get_repositories(provider):
+        """Display list of the user's repositories."""
+        svc = VCSService.for_provider_and_user(provider, current_user.id)
+        ctx: dict = dict(
+            connected=False,
+            provider=provider,
+            vocabulary=svc.provider.factory.vocabulary,
+            repo_url=svc.provider.factory.url_for_repository,
+            new_repo_url=svc.provider.factory.url_for_new_repo(),
+        )
+
+        if svc.is_authenticated:
+            # Generate the repositories view object
+            repos = svc.list_repositories()
+            last_sync = svc.get_last_sync_time()
+
+            ctx.update(
+                {
+                    "connected": True,
+                    "repos": repos,
+                    "last_sync": last_sync,
+                }
+            )
+
+        return render_template(current_app.config["VCS_TEMPLATE_INDEX"], **ctx)
+
+    @blueprint.route("/repository/<path:vcs_repo_id>")
+    @login_required
+    @require_vcs_connected()
+    @vcs_error_handler()
+    def get_repository(provider, vcs_repo_id):
+        """Displays one repository.
+
+        Retrieves and builds context to display all repository releases, if any.
+        """
+        svc = VCSService.for_provider_and_user(provider, current_user.id)
+
+        db_repo = svc.get_repository(vcs_repo_id)
+        latest_release = svc.get_repo_latest_release(db_repo.id)
+        default_branch = svc.get_repo_default_branch(vcs_repo_id)
+        releases = svc.list_repo_releases(db_repo)
+        release_url = svc.provider.factory.url_for_release
+        new_release_url = svc.provider.factory.url_for_new_release(db_repo.full_name)
+        new_citation_file_url = svc.provider.factory.url_for_new_file(
+            db_repo.full_name, default_branch or "main", "CITATION.cff"
+        )
+
+        return render_template(
+            current_app.config["VCS_TEMPLATE_VIEW"],
+            latest_release=latest_release,
+            provider=provider,
+            repo=db_repo,
+            releases=releases,
+            default_branch=default_branch,
+            release_url=release_url,
+            new_release_url=new_release_url,
+            new_citation_file_url=new_citation_file_url,
+            vocabulary=svc.provider.factory.vocabulary,
+        )
+
+
+def register_api_routes(blueprint):
+    """Register API routes."""
+
+    @login_required
+    @require_vcs_connected()
+    @vcs_error_handler()
+    @blueprint.route("/repositories/sync", methods=["POST"])
+    def sync_user_repositories(provider):
+        """Synchronizes user repos.
+
+        Currently:
+            POST /api/user/vcs/<provider>/repositories/sync
+        Previously:
+            POST /account/settings/github/hook
+        """
+        svc = VCSService.for_provider_and_user(provider, current_user.id)
+        svc.sync()
+        db.session.commit()
+
+        return "", 200
+
+    @login_required
+    @require_vcs_connected()
+    @vcs_error_handler()
+    @blueprint.route("/repositories/<repository_id>/enable", methods=["POST"])
+    def enable_repository(provider, repository_id):
+        """Enables one repository.
+
+        Currently:
+            POST /api/user/vcs/<provider>/repositories/<repository_id>/enable
+        Previously:
+            POST /account/settings/github/hook
+        """
+        community_id = request.args.get("community_id")
+        svc = VCSService.for_provider_and_user(provider, current_user.id)
+        create_success = svc.enable_repository(repository_id, community_id=community_id)
+
+        db.session.commit()
+        if create_success:
+            return "", 201
+        else:
+            raise Exception(
+                _("Failed to enable repository, hook creation not successful.")
+            )
+
+    @login_required
+    @require_vcs_connected()
+    @vcs_error_handler()
+    @blueprint.route("/repositories/<repository_id>/disable", methods=["POST"])
+    def disable_repository(provider, repository_id):
+        """Disables one repository.
+
+        Currently:
+            POST /api/user/vcs/<provider>/repositories/<repository_id>/disable
+        Previously:
+            DELETE /account/settings/github/hook
+        """
+        svc = VCSService.for_provider_and_user(provider, current_user.id)
+        remove_success = svc.disable_repository(repository_id)
+
+        db.session.commit()
+        if remove_success:
+            return "", 204
+        else:
+            raise Exception(
+                _("Failed to disable repository, hook removal not successful.")
+            )