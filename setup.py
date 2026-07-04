721d318a432a594371a63e68ce81fa8ba7c4932b
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2026-07-04 23:50:07 +0200

    chore(setup): migrate from setuptools to hatchling
    * remove MANIFEST.in because that's only relevant for setuptools
    * translate the i18n/translation mechanisms as well
    * declare conflicting extras for uv
    * format pyproject.toml with Tombi
    
    Co-authored-by: Sam Arbid <samaa2@kth.se>

--- setup.py
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: 2023-2026 CERN.
-# SPDX-FileCopyrightText: 2023 Graz University of Technology.
-# SPDX-License-Identifier: MIT
-
-"""Invenio module that adds VCS integration to the platform."""
-
-from setuptools import setup
-
-setup()
