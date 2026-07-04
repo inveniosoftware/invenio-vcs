a032f9ffa3c7bcbef93bd59afea76774977ae0c4
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2026-07-05 00:01:29 +0200

    chore(format): fix black and isort complaints

--- invenio_vcs/views/vcs.py
+++ invenio_vcs/views/vcs.py
@@ -67,7 +67,9 @@ def create_ui_blueprint(app):
         template_folder="../templates",
         url_prefix="/account/settings/vcs/<provider>",
     )
-    with app.app_context():  # Todo: Temporary fix, it should be removed when inveniosoftware/invenio-theme#355 is merged
+    with (
+        app.app_context()
+    ):  # Todo: Temporary fix, it should be removed when inveniosoftware/invenio-theme#355 is merged
         register_ui_routes(blueprint)
     return blueprint
 
